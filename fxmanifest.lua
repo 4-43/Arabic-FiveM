a26dea425ba983824ad04a3c5f55d25b2c20eed4
Rᴀᴍʏ <117773187+4-43@users.noreply.github.com>
2023-12-19 22:48:44 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'RAMY#9442'
+description 'Script تعريب المنيو'
+version '1.0' -- النسخه
+
+resource_type 'gametype' { name = 'Script Ramy#9442' }
+
+--client_script 'pausemenu.lua'
+
+client_scripts {
+	'client/main.lua',
+	'pausemenu.lua'
+}
+
+this_is_a_map 'yes'
